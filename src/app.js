d78f47206bf302c2d4930b6b30b1524ba6b089a7
Sundeep Kumar <sundeep6896@gmail.com>
2019-04-08 14:45:57 +0500

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -1,94 +1,95 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast');
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Sundeep Kumar'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Sundeep Kumar"
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Sundeep Kumar'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Sundeep Kumar"
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Sundeep Kumar'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Sundeep Kumar"
+  });
+});
 
-app.get('/weather', (req, res) => {
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Please provide address"
+    });
+  }
 
-    if(!req.query.address){
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
         return res.send({
-            error : 'Please provide address'
-        })
-    }
+          error
+        });
+      }
 
-    geocode(req.query.address, (error, {latitude,longitude,location} = {}) => {
+      forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-            return res.send({
-                error
-            })
+          return res.send({
+            error
+          });
         }
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address
+        });
+      });
+    }
+  );
+});
 
-        forecast(latitude,longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({
-                    error
-                })
-            }
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Sundeep Kumar",
+    errorMessage: "Help article not found."
+  });
+});
 
-    
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Sundeep Kumar",
+    errorMessage: "Page not found."
+  });
+});
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Sundeep Kumar',
-        errorMessage: 'Help article not found.'
-    })
-})
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Sundeep Kumar',
-        errorMessage: 'Page not found.'
-    })
-})
-
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
